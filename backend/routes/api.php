82386637058d36a97b6a8298b383d0f23f32f7aa
mathesukkj <kemuel.g7363@gmail.com>
2024-05-11 12:32:34 -0300

    feat: add city model + migration

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,3 +11,6 @@ Route::get('/user', function (Request $request) {
 Route::post('/signup', [AuthController::class, 'signup']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware("auth:sanctum")->group(function () {
+});
