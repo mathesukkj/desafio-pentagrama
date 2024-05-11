dc1801069a11a5074e3412bc60c54a96df0ba954
mathesukkj <kemuel.g7363@gmail.com>
2024-05-11 15:39:13 -0300

    feat: add custom 404 message

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        });
     })->create();
