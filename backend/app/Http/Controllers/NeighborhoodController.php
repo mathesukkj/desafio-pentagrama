6041e4800c19cb7e384c2681506d83e570cfd646
mathesukkj <kemuel.g7363@gmail.com>
2024-05-11 13:16:50 -0300

    feat: add crud for neighborhoods

---
+++ backend/app/Http/Controllers/NeighborhoodController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Neighborhood;
+use Illuminate\Http\Request;
+
+class NeighborhoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Neighborhood::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $neighborhood = Neighborhood::create([
+            "name" => $request->input("name"),
+            "city_id" => $request->input("city_id"),
+        ]);
+
+        return $neighborhood;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $neighborhood = Neighborhood::findOrFail($id);
+
+        return $neighborhood;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $neighborhood = Neighborhood::findOrFail($id);
+
+        $neighborhood->name = $request->input("name");
+        $neighborhood->city_id = $request->input("city_id");
+        $neighborhood->save();
+
+        return $neighborhood;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $neighborhood = Neighborhood::findOrFail($id);
+
+        $neighborhood->delete();
+
+        return response()->json([], 204);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CityController;
+use App\Http\Controllers\NeighborhoodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware("auth:sanctum")->group(function () {
     Route::resource('/cities', CityController::class);
+    Route::resource('/neighborhoods', NeighborhoodController::class);
 });
